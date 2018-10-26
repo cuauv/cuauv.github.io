fedc209b7aa6ea24d1d86b67ab610c63d0eb2aac
Rishab Bhandari <rishabbhandari@dhcp-rhodes-3570.redrover.cornell.edu>
2018-10-26 15:57:12 -0400

    team page

---
+++ _site/default.nix
@@ -0,0 +1,15 @@
+with import <nixpkgs> { };
+
+let jekyll_env = bundlerEnv rec {
+    name = "jekyll_env";
+    gemdir  = ./.;
+  };
+in
+  stdenv.mkDerivation rec {
+    name = "jekyll_env";
+    buildInputs = [ jekyll_env nodejs ];
+
+    shellHook = ''
+      exec ${jekyll_env}/bin/jekyll serve --watch --incremental
+    '';
+  }
